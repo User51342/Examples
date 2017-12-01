b92bf3c7d7ff0354d103e079c993a38a41c5c600
User51342 <31661795+User51342@users.noreply.github.com>
2017-12-01 12:35:09 +0100

    Add project files.

---
+++ ThreadSafeSingletonExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSafeSingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.Instance;
+            Console.WriteLine($"Singleton created at {singleton.ToString()}");
+            Thread.Sleep(3000);
+            Console.WriteLine($"Singleton created at {singleton.ToString()}");
+            Thread.Sleep(3000);
+            Console.WriteLine($"Singleton created at {singleton.ToString()}");
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ ThreadSafeSingletonExample/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreadSafeSingletonExample
+{
+    public class Singleton
+    {
+        private DateTime created;
+        private Singleton()
+        {
+            created = DateTime.Now;
+        }
+        private DateTime Created { get { return Created; } }
+        public static Singleton Instance { get { return Nested.instance; } }
+
+        public override string ToString()
+        {
+            return created.ToString();
+        }
+        private class Nested
+        {
+            static Nested()
+            {
+            }
+
+            internal static Singleton instance = new Singleton();
+        }
+    }
+}
