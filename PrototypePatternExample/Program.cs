42ed00b09f2870ec8e0ba71d869f4ea523a80838
User51342 <31661795+User51342@users.noreply.github.com>
2017-12-05 14:11:03 +0100

    Protoype pattern example added.

---
+++ PrototypePatternExample/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PrototypePatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var status1 = new StatusSwitcher();
+            status1.DisplayStatus();
+            status1.SwitchStatusTo(2);
+            status1.DisplayStatus();
+            var status2 = status1.Clone() as StatusSwitcher;
+            status2.DisplayStatus();
+            status2.SwitchStatusTo(3);
+            status1.DisplayStatus();
+            status2.DisplayStatus();
+        }
+    }
+}
---
+++ PrototypePatternExample/StatusSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PrototypePatternExample
+{
+    public class StatusSwitcher : ICloneable
+    {
+        #region Fields
+        private int status;
+        #endregion
+
+        #region Properties
+        public int Status
+        {
+            get { return status; }
+            private set { status = value; }
+        }
+        #endregion
+
+        #region Construction / Initialization / Deconstruction
+        public StatusSwitcher()
+        {
+            Status = 1;
+        }
+        #endregion
+
+        #region Public implementation
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"Status={Status}");
+        }
+
+        public void SwitchStatusTo(int newStatus)
+        {
+            Status = newStatus;
+        }
+        #endregion
+    }
+}
